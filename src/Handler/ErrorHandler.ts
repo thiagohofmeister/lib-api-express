2109e28f9e1bc2e366f6e20b654d97a67a12c467
Thiago Hofmeister <thiago.souza@moovin.com.br>
2020-05-15 01:00:50 -0300

    Add project Handlers (default Contract and Error)

---
+++ src/Handler/ErrorHandler.ts
@@ -0,0 +1,67 @@
+import { Request, Response, NextFunction } from 'express'
+import { NotAllowed, AlreadyExists, DataNotFound, InvalidData, InvalidUserAuthenticationData } from '../Response'
+
+export class ErrorHandler {
+  public constructor () {
+    this.notFound = this.notFound.bind(this)
+    this.error = this.error.bind(this)
+  }
+
+  public notFound (request: Request, response: Response, next: NextFunction) {
+    return this.error(new DataNotFound('001', 'Resource not found.'), request, response, next)
+  }
+
+  public error (error: Error, request: Request, response: Response, next: NextFunction) {
+    let httpCode = 500
+    let body = {
+      code: `${httpCode}.001`,
+      message: error.message,
+      details: []
+    }
+
+    if (error instanceof InvalidUserAuthenticationData) {
+      httpCode = 401
+      body = {
+        ...body,
+        code: `${httpCode}.${error.code}`,
+        details: error.details
+      }
+    } else if (error instanceof NotAllowed) {
+      httpCode = 403
+      body = {
+        ...body,
+        code: `${httpCode}.${error.code}`,
+        details: error.details
+      }
+    } else if (error instanceof DataNotFound) {
+      httpCode = 404
+      body = {
+        ...body,
+        code: `${httpCode}.${error.code}`,
+        details: error.details
+      }
+    } else if (error instanceof AlreadyExists) {
+      httpCode = 409
+      body = {
+        ...body,
+        code: `${httpCode}.${error.code}`,
+        details: error.details
+      }
+    } else if (error instanceof InvalidData) {
+      httpCode = 422
+      body = {
+        ...body,
+        code: `${httpCode}.${error.code}`,
+        details: error.details
+      }
+    }
+
+    if (httpCode === 500) {
+      console.error(error)
+    }
+
+    response
+      .status(httpCode)
+      .json(body)
+  }
+}
